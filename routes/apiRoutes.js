84cd2d1360150b0ca124098cb837b63a19fb1376
Amanda Nelson <amandaln4311@gmail.com>
2021-01-25 17:12:30 -0500

    cleaned up code and pages

---
+++ routes/apiRoutes.js
@@ -0,0 +1,25 @@
+var path = require("path");
+const fs = require("fs");
+
+module.exports = function(app) {
+
+    // console.log("test")
+    fs.readFile(__dirname, "/db/db.json", "utf-8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+    });
+  app.post("/api/notes", function (req, res) {
+    
+    });
+    app.get("/api/notes", function (req, res) {
+        fs.readFile(__dirname, "/db/db.json", "utf-8", function (error, data) {
+            if (error) {
+                return console.log(error);
+            }
+            console.log("Note: ", data)
+            res.json(JSON.parse(data))
+        });
+    });
+ 
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+// Sets up the Express App
+const app = express();
+//process.env - property returns an object containing the user environment.
+const PORT = process.env.Port || 3000;
+// Sets up the Express app to handle data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//Static files and resources
+app.use(express.static("public"));
+
+
+
+require("./routes/apiRoutes")(app)
+require("./routes/htmlRoutes")(app)
+
+
+
+       
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+        
+    
